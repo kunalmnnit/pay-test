64a04a0376db16972d266354a79be37ae6d5bd1d
kunalmnnit <ksevkani495@gmail.com>
2023-03-12 11:40:08 +0530

    Add Webhook URL

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require("express");
 const con = require("./db");
 const app = express();
 const cors = require("cors");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
 app.use(
   cors({
     origin: "*",
@@ -27,6 +31,12 @@ app.get("/api/token", (req, res) => {
       }
     );
   });
+});
+
+app.post("/webhook", (req, res) => {
+  // Handle the webhook request here
+  console.log(req.body);
+  res.status(200).send("Webhook received");
 });
 
 app.listen(3000, () => {
